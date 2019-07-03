c67f665f9d32999b0ea83d815e91d0adbfa7338b
Brian Sayler <sayler.b@gmail.com>
2019-07-03 14:34:01 -0500

    sanity check test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, cleanup } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+test("renders without crashing", () => {
+  const { getByText } = render(<App />);
+
+  const text = getByText("Hello, World!");
+
+  expect(text).toHaveClass("greeting");
 });
